c3c6033479e28e61a17d59c03caa14664e282865
Ben <benjamin.napper17@gmail.com>
2025-08-13 21:56:43 -0400

    third attempt

--- src/app/(crm)/jobs/[jobId]/estimate/pdf/route.tsx
+++ src/app/(crm)/jobs/[jobId]/estimate/pdf/route.tsx
@@ -9,7 +9,7 @@ export async function  GET(request: Request, { params }: { params: Promise<{jobI
   const job = await getJob(Number(jobId))
   const client = await getClient(job.clientId)
 
-   const logoPath = process.env.NODE_ENV === 'production' ? '/images/logo.png' : 'public/images/logo.png';
+   const logoPath = process.env.NODE_ENV === 'production' ? 'images/logo.png' : 'public/images/logo.png';
 
   const styles = StyleSheet.create({
     header: {
